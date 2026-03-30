56b15bfdfe56331b96ad28df4a77525f0fce6169
BatsuraBohdana <batsura.bohdana@chnu.edu.ua>
2026-03-30 19:46:58 +0300

    Clean up Triangle.cs by removing comments
    Removed commented-out sections for indexer and operators.

--- Lab4CSharp/Triangle.cs
+++ Lab4CSharp/Triangle.cs
@@ -13,7 +13,6 @@ namespace Lab4CSharp
             color = triangleColor;
         }
 
-        // Індексатор (Завдання 1.1)
         public int this[int index]
         {
             get => index switch
@@ -34,18 +33,14 @@ namespace Lab4CSharp
             }
         }
 
-        // Оператори ++ та -- (Завдання 1.2)
         public static Triangle operator ++(Triangle t) { t.a++; t.b++; t.c++; return t; }
         public static Triangle operator --(Triangle t) { t.a--; t.b--; t.c--; return t; }
 
-        // Оператори true/false (Завдання 1.3)
         public static bool operator true(Triangle t) => (t.a + t.b > t.c) && (t.a + t.c > t.b) && (t.b + t.c > t.a);
         public static bool operator false(Triangle t) => !((t.a + t.b > t.c) && (t.a + t.c > t.b) && (t.b + t.c > t.a));
 
-        // Оператор * на скаляр (Завдання 1.4)
         public static Triangle operator *(Triangle t, int scalar) => new Triangle(t.a * scalar, t.b * scalar, t.c * scalar, t.color);
 
-        // Перетворення типів (Завдання 1.5)
         public static implicit operator string(Triangle t) => $"Triangle: a={t.a}, b={t.b}, c={t.c}, color={t.color}";
         public static explicit operator Triangle(string s)
         {
